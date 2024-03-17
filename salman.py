7be4f48b839745c16f57e9298e9da544b6d42f4c
Salman <salmanap46@gmail.com>
2024-03-17 15:04:01 +0530

    first commit

---
+++ salman.py
@@ -0,0 +1,118 @@
+#A
+
+#salman
+
+
+#**********************************
+
+#1
+"""def main(n):
+    num=1
+    for i in range(1,n+1):
+        for j in range(i):
+            print(num,end =' ')
+            num+= 1
+        print()
+        
+main(5)"""
+
+
+n = 6
+m = 1
+for i in range(1, n+1):
+    for j in range(i):
+        print(m,end=' ')
+        m += 1
+    print()
+    
+
+#5
+
+'''string = input('Enter the string ')
+a = 'ing'
+b = 'ly'
+def main(st):
+    length = len(st)
+    if length < 3:
+        ans = st
+        print(st[0])
+    elif st[length-1]=='g' and st[length-2] == 'n' and st[length-3] == 'i':
+        ans = st+b
+    else:
+        ans = st+a
+    return ans
+
+string_ans = main(string)
+print(string_ans)'''
+
+#6
+
+'''string = input('Enter your string ')
+string = list(string)
+for i in string:
+    if i == '*' or i == '^' or i == '%' or i == '!' or i == '%' or i == '#':
+        string.remove(i)
+string = ''.join(string)
+print(string)'''
+
+#4
+
+'''a1 = [1,2,3,5,7,8,9,10]
+b1 = [1,2,4,8,9]
+
+a2 = set(a1)
+b2 = set(b1)
+
+main = lambda a,b: list(sorted((a & b)))
+
+print(main(a2,b2))'''
+
+#2
+
+"""sub = {'math':81,'physics':83,'chemistry':87}
+
+main = {}
+
+if sub.value[0] > sub.value[1] and sub.value[2]:
+    main."""
+
+#3
+"""sub = {}
+def convert(a):
+    for i,m in a.items():
+        m = int(m)
+        sub[i] = m
+        
+        
+        
+    return a
+
+main = {'a':'5','b':'7','c':'8'}
+
+res = convert(main)
+print(res)"""
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+        
+
+
+
+
+
+
+    
